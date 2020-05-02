86ba5d29834a79987724438ae8295bc274390b4c
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-01 23:54:42 -0400

    implemented axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// intercepts every request sent using axios. (common config)
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config. headers ect..
+    return request;
+}, error => { // network errors
+    console.log(error);
+    return Promise.reject(error);
+});
+
+// intercepts responses 
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => { // response errors
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();

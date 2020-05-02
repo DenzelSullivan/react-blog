b3b6c0c9e8e04ce90b7ebe40c6cc4d2a6c6deeb1
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-02 00:11:03 -0400

    Added axios default config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+// setting base url
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+//example of setting and auth token and content type
+// json is already default.
+//axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 // intercepts every request sent using axios. (common config)
 axios.interceptors.request.use(request => {
     console.log(request);
